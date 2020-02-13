0b369456710cb5b6bc79e3a84cbd3b1b785faae0
Ramil Fatykhov <ramil.fatykhov@citronium.com>
2020-02-13 12:56:02 +0300

    small refactoring

--- public/client.js
+++ public/client.js
@@ -1,17 +1,21 @@
-strings = {
+const returnStrings = (msg) => {
+	const strings = {
 	'connected': '[sys][time]%time%[/time]: Вы успешно соединились как [user]%name%[/user].[/sys]',
 	'userJoined': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] присоединился к чату.[/sys]',
 	'messageSent': '[out][time]%time%[/time]: [user]%name%[/user]: %text%[/out]',
 	'messageReceived': '[in][time]%time%[/time]: [user]%name%[/user]: %text%[/in]',
 	'userSplit': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] покинул чат.[/sys]'
-};
+	}
+	// eslint-disable-next-line no-useless-escape
+	return strings[msg.event].replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
+}
 
 window.onload = function() {
 	socket = io.connect('localhost:3000');
 	socket.on('connect', function () {
 		socket.on('message', function (msg) {
 			// eslint-disable-next-line no-useless-escape
-			document.querySelector('#log').innerHTML += strings[msg.event].replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
+			document.querySelector('#log').innerHTML += returnStrings(msg);
 			document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
     });
     
